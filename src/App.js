0a83b407a919425499537749bfa4f020253776e8
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2023-11-27 11:58:55 -0300

    implemented the useEffect effect in the project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import axios from "axios"
 import People from "./assets/people.svg"
 import Arrow from "./assets/arrow.svg"
@@ -18,20 +18,28 @@ import {
 } from "./styles"
 
 
-const App = () => {
+function App() {
 
   const [users, setUsers] = useState([])
   const inputName = useRef()
   const inputCpf = useRef()
 
   async function addNewUsers() {
+
     const { data: newUser } = await axios.post("http://localhost:3001/users", {
       name: inputName.current.value, cpf: inputCpf.current.value
     });
-    setUsers([...users, newUser])
-    console.log(newUser)
 
+    setUsers([...users, newUser])
   }
+  useEffect(() => {
+    async function fetchUsers() {
+      const { data: newUsers } = await axios.get("http://localhost:3001/users")
+      setUsers(newUsers)
+    }
+
+    fetchUsers()
+  }, [])
 
   function deleteUser(userId) {
     const newUsers = users.filter((user) => user.id !== userId)
@@ -56,25 +64,16 @@ const App = () => {
         </Button>
 
         <ul>
-          {users.map((users) => (
-
-            <User key={users.id}>
-
-              <p>{users.name}</p> <p> {users.cpf}</p>
-
-              <Button2 onClick={() => deleteUser(users.id)}>
-
-                <img alt="Trash" src={Trash}></img>
-
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p> {user.cpf}</p>
+              <Button2 onClick={() => deleteUser(user.id)}>
+                <img alt="Trash" src={Trash} />
               </Button2>
-
             </User>
           ))}
         </ul>
-
-
       </ContainerItens>
-
     </Container>)
 }
 
